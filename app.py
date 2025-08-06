da9a45914c4f26596cd40299d821aaef13fa2da4
henriquegalva0 <henriquegalvao@discente.ufg.br>
2025-08-05 23:32:34 -0300

    Trocando base do deepseek pelo modelo mistral e elaborando teste com ideia de mercado.

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
 from flask import Flask, render_template, request
-from callbot import generate_answer
+from llama_cpp import Llama
+import re
+
+caminho = "./models/mistral-7b-instruct-v0.2.Q4_K_M.gguf"
+
+chatbot = Llama(
+    model_path=caminho,
+    n_ctx=2048,
+    n_threads=6,
+    verbose=False,
+)
 
 app = Flask(__name__)
-
-
-chat_history=[{"role":"User","content":"input"},{"role":"Bot","content":"output"}]
-user_input=""
 
 @app.route("/",methods=["GET","POST"])
 def index():
 
-    global user_input, chat_history
-
     if request.method=="POST":
-
-        user_input=request.form.get("user_input")
-        chat_history=generate_answer(user_input,chat_history)
-
-    return render_template("index.html", display=chat_history)
+        return render_template("index.html", message="trabalhando")
     
 if __name__ == "__main__":
     app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,27 +1,37 @@
 from llama_cpp import Llama
+import re
 
-model_path = "./models/deepseek-llm-7b-chat.Q5_K_M.gguf"
+caminho = "./models/mistral-7b-instruct-v0.2.Q4_K_M.gguf"
 
-llm = Llama(
-    model_path=model_path,
+chatbot = Llama(
+    model_path=caminho,
     n_ctx=2048,
-    n_threads=8,
-    verbose=True,
+    n_threads=6,
+    verbose=False,
 )
 
-chat_history=[]
-user_input="What is Pi in the math context?"
+historico_chat = """<|system|>
+Seu perfil:\n Seu nome é "Flokebot" e você é o assistente virtual responsável pelo atendimento do mercado 'Floke'. Você deve responder de modo carismático, atencioso e sempre em português.\n
+Sua missão:\n Você deve responder dúvidas de forma humanizada, correta e coerente a respeito de informações do mercado.\n
+Sobre o mercado:\n O mercado se chama 'Floke'. O mercado funciona das 9 horas da manhã até as 21 horas da noite. O mercado foi fundado na data 21/05/2003. No estoque do mercado há: 30 frutas, 21 produtos de limpeza, 50 bebidas diferentes e 28 legumes. O mercado não disponibiliza reembolso.\n
+Observações:\n Não utilize linguagem técnica!\n Não responda dúvidas que não sejam relacionadas ao mercado!\n Sempre fale bem da infraestrutura e da confiança do mercado!\n Responda somente a pergunta, ou seja, não fale informações que não tenham relação com a pergunta.\n Utilize linguagem compreensível de acordo com a norma comum da lingua portuguesa.\n
+<|end|>\n
+"""
 
-chat_history.append({"role":"user","content":user_input})
+while True:
+    input_usuario = input(" Você: ")
+    if input_usuario.lower() in ["sair", "exit", "quit"]:
+        break
 
-gen_output=llm.create_chat_completion(
-    messages=chat_history,
-    temperature=0.45,
-    top_p=0.95,
-)
+    historico_chat += f"<|user|>\n{input_usuario}\n<|end|>\n<|assistant|>\n"
 
-bot_output=gen_output["choices"][0]["message"]["content"]
-chat_history.append({"role":"assistant","content":bot_output})
+    chamar_resposta = chatbot(historico_chat, max_tokens=512, stop=["<|end|>"])
+    escolher_resposta = chamar_resposta['choices'][0]['text'].strip()
+    output_chatbot = re.split(r"<\|user\|>", escolher_resposta)[0].strip()
 
-for key,item in chat_history.items():
-    print(f"{key}: ",item,end="\n")
+    print(f"""
+    👤 Você: {input_usuario}
+    🤖 Flokebot: {output_chatbot}
+    """)
+
+    historico_chat += output_chatbot + "\n<|end|>\n"
